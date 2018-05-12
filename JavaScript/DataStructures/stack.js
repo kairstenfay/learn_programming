313b71bdda3e2c91a284251883713e5767fdbfae
KairstenFay <kairsten.fay@gmail.com>
2018-05-12 14:39:25 -0700

    done with stack for now

--- JavaScript/DataStructures/stack.js
+++ JavaScript/DataStructures/stack.js
@@ -28,8 +28,8 @@ I'll provide a test suite for the data structure and the problem (thanks Falko!)
 
 
 class Stack {
-  constructor(list=[], maxSize=10) {
-    this._list = list;
+  constructor(maxSize=10) {
+    this._list = [];
     this._maxSize = maxSize;
     this._curlyBalance = 0;
     this._squareBalance = 0;
@@ -39,6 +39,7 @@ class Stack {
     return this._list;
   }
   set list(someArray) {
+    this._updateBalance();
     return this._list = someArray;
   }
   get maxSize () {
@@ -52,19 +53,20 @@ class Stack {
       return this._maxSize;
     }
   }
-  pop () {
-    // removes the first item from the stack and returns the new stack
+  pop () { // TODO: test to make sure length is one less than before
+    // removes the top item from the stack and returns the new stack
     if (this._list.length < 1) {
       throw new RangeError('There is nothing left to pop!');
     } else {
-      return this._list.pop();
+      this._list.pop();
+      this._updateBalance();
+      return this._list;
     }
   }
   push (newItem) {
     // adds an item to the top of the stack
     this._list.push(newItem);
-    // check new item
-    this._updateBalance(this._list[this._list.length-1]);
+    this._updateBalance();
 
     if (this._list.length > this._maxSize) {
       throw new RangeError(`You have exceeded the stack limit of ${this._maxSize}!`);
@@ -73,47 +75,58 @@ class Stack {
     }
   }
   peek () {
+    // makes a copy of the top item in the stack
     if (this._list.length < 1) {
       throw new RangeError('There is nothing in the stack to see!');
     } else {
       return this._list[this._list.length - 1];
     }
   }
-  _updateBalance (listItem) {
-    /*
-    switch (this._list.includes()) {
-      case '{':
-        return this._curlyBalance++;
-      case '(':
-        return this._curvyBalance++;
-      case '[':
-        return this._squareBalance++;
-      case '}':
-        return this._curlyBalance--;
-      case ')':
-        return this._curvyBalance--;
-      case ']':
-        return this._squareBalance--;
-        */
-    // for (let i = 0; i < listItem; i++) {
-      console.log('Evaluating ' + listItem);
-      if (this._list[i].includes('{')) {
-        this._curlyBalance++;
-      } else if (this._list[i].includes('(')) {
-        this._curvyBalance++;
-      } else if (this._list[i].includes('[')) {
-        this._squareBalance++;
-      } else if (this._list[i].includes('}')) {
-        this._curlyBalance--;
-      } else if (this._list[i].includes(')')) {
-        this._curvyBalance--;
-      } else if (this._list[i].includes(']')) {
-        this._squareBalance--;
-      }
+  _updateBalance () {
+    // updates the balance by resetting it then checking for matches to the parentheses using a nested for loop
+    this._curlyBalance = 0;
+    this._curvyBalance = 0;
+    this._squareBalance = 0;
 
-     // for (let j = 0; j < this._list[i].length; j++) {
-     //  while (this._list[i][j] )
-     }
+    for (let i = 0; i < this._list.length; i++) {
+      for (let j = 0; j < this._list[i].length; j++) {
+        switch(this._list[i][j]) {
+          case '{':
+            this._curlyBalance++;
+            break;
+
+          case '(':
+            this._curvyBalance++;
+            break;
+
+          case '[':
+            this._squareBalance++;
+            break;
+
+          case '}':
+            this._curlyBalance--;
+            break;
+
+          case ')':
+            this._curvyBalance--;
+            break;
+
+          case ']':
+            this._squareBalance--;
+            break;
+        }
+      }
+    }
+  }
+   checkBalance () {
+    this._updateBalance();
+    if (this._curvyBalance !== 0 || this._curlyBalance !== 0 || this._squareBalance !== 0) {
+      // throw new SyntaxError('Your parentheses are not balanced!');
+      return "Your parentheses are not balanced!"
+    } else {
+      return 'You are balanced.';
+    }
+   }
 
 /*  static pickSubstituteTeacher(substituteTeachers) {
     const rand = Math.floor(Math.random() * substituteTeachers.length);
@@ -123,30 +136,28 @@ class Stack {
 
 }
 
-const list = [1,2,3];
-
-let test = new Stack();
-test.list = list;
-console.log(test.list);
-test.pop();
-console.log(test.list);
-
-const list2 = [1,2,3,4,5];
-let test2 = new Stack(list2);
-
-console.log(test2);
-test2.maxSize = 5;
-test2.list = list2;
-console.log(test2.list);
-console.log(test2.maxSize);
-// console.log(test2.push(6));
-// test2.maxSize = 4;
-
-test2.peek();
-const temp = test2.peek();
-console.log(temp); // makes a copy
-
-console.log(test2.list);
+// const list = [1,2,3];
+//
+// let test = new Stack();
+// test.list = list;
+// console.log(test.list);
+// test.pop();
+// console.log(test.list);
+//
+// const list2 = [1,2,3,4,5];
+// let test2 = new Stack();
+// test.list = list2
+//
+// console.log(test2);
+// test2.maxSize = 99;
+// console.log(test2.list);
+// console.log(test2.maxSize);
+// // console.log(test2.push(6));
+// // test2.maxSize = 4;
+//
+// const temp = test2.peek();
+// console.log(temp); // makes a copy
+// console.log(test2.list);
 
 
 let test3 = new Stack();
@@ -154,11 +165,40 @@ let test3 = new Stack();
 test3.push('3');
 test3.push('4');
 test3.push('5');
-console.log(test3.list);
-console.log('\n\n');
-console.log("Testing balance function");
 test3.push('{');
+test3.push('}(');
+test3.push('(');
 test3.push('}');
-
-// console.log(test3.list.includes('{'));
 console.log(test3);
+console.log(test3.checkBalance());
+test3.pop();
+console.log(test3.checkBalance());
+console.log(test3);
+console.log('\n\n');
+
+let test4 = new Stack();
+test4.push('A curly eyebrow emoticon is drawn like this: }:)                {(');
+console.log(test4);
+console.log(test4.checkBalance());
+
+console.log('\n\n');
+
+// todo: update balance when updating list or creating instance of new Stack with list passed in
+// I removed the `list` argument from the constructor so that every new instance starts
+// with an empty stack
+
+
+console.log('Todo?');
+let newList = ['}{{{}(('];
+let test5 = new Stack();
+test5.list = newList;
+// console.log(test5); // does not reflect balance
+// test5.list = newList;
+console.log(test5); // using the setter does
+console.log(test5.checkBalance());
+console.log(test5);
+console.log(test5.peek());
+
+
+// todo: add tests to make sure that the balances are correct
+// todo: make sure checkBalance works
